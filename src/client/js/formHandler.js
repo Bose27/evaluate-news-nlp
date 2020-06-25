f4d87d2f337a86292b8db16e610a46aa0d53cc66
Pinki Bose <pbose@adobe.com>
2020-06-25 00:11:47 -0700

    implemented review1

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,21 +1,10 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-let baseURL = "https://api.aylien.com/api/v1/sentiment";
-
-var aylien = require("aylien_textapi");
-// set aylien API credentias
-var textapi = new aylien({
-  application_id: process.env.API_ID,
-  application_key: process.env.API_KEY,
-});
-
 var el = document.querySelector("input.submit");
 document.addEventListener("DOMContentLoaded", function () {
   el.addEventListener("submit", handleSubmit, false);
 });
 
 function handleSubmit(e) {
+  e.preventDefault();
   // check what text was put into the form field
   let formText = document.querySelector("#text").value;
   console.log(formText);
@@ -33,7 +22,13 @@ function handleSubmit(e) {
       console.log("Aylien results listed below");
       console.log(response);
       // Adding results into HTML
-      document.querySelector("#results").innerHTML = response.polarity;
+      document.getElementById("your-text").innerHTML = response.text;
+      document.getElementById("polarity").innerHTML = response.polarity;
+      document.getElementById("polarity_confidence").innerHTML =
+        response.polarity_confidence;
+      document.getElementById("subjectivity").innerHTML = response.subjectivity;
+      document.getElementById("subjectivity_confidence").innerHTML =
+        response.subjectivity_confidence;
     });
 }
 
