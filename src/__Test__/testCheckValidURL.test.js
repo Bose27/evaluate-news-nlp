0fc6e99b5915341c19ec13626e63b90875f10b0f
Pinki Bose <pbose@adobe.com>
2020-06-27 00:52:27 -0700

    upadated tests

--- src/__Test__/testCheckValidURL.test.js
+++ src/__Test__/testCheckValidURL.test.js
@@ -1,5 +1,5 @@
 // Import the js file to test
-import { checkValidURL } from "../client/js/checkValidURL";
+import { validURL } from "../client/js/checkValidURL";
 
 describe("Testing the submit functionality", () => {
   // The test() function has two arguments - a string description, and an actual test as a callback function.
